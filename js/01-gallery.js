cb66e0b2c7c47f258d6a083a60fbf6930522d03f
KureninovArtem <97453414+KureninovArtem@users.noreply.github.com>
2022-07-18 21:25:39 +0300

    FinishTask-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const gallery = greateGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", gallery);
+galleryContainer.addEventListener("click", clickGallery);
+
+function greateGallery(array) {
+    return array.reduce(
+        (acc, item) =>
+            acc + `<div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+            />
+            </a>
+            </div>`,
+        "");
+};
+
+function clickGallery(event) {
+    event.preventDefault();
+
+    const modal = basicLightbox.create(
+        `<img src="${event.target.dataset.source}" width="800" height="600">`,
+        {
+            onShow: () => window.addEventListener("keydown", pressEsc),
+            onClose: () => window.removeEventListener("keydown", pressEsc),
+        }
+    );
+
+    modal.show();
+
+    function pressEsc (event) {
+        if (event.code === "Escape") {
+            modal.close();
+        }
+    };
+};
